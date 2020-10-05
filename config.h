fb5730f85ad34508d9d4b22ad6fe7e131b15b5f7
GregCM <gregcaceres@gmail.com>
2020-10-04 22:10:05 -0500

    post-make

--- config.h
+++ config.h
@@ -14,6 +14,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeHp] = { "#bbbbbb", "#333333" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;

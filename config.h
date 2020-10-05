33fa66c34381e9ef7dae9f72e1043b38257c0354
GregCM <gregcaceres@gmail.com>
2020-10-04 22:05:41 -0500

    appended Center patch

--- config.h
+++ config.h
@@ -1,7 +1,9 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                 /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
